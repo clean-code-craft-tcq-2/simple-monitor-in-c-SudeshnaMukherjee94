a6da2dd748b2746615ce44d2061fc371a0d71cf5
SudeshnaMukherjee94 <91111960+SudeshnaMukherjee94@users.noreply.github.com>
2022-02-11 01:45:53 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -9,9 +9,9 @@ int CheckRange (float Value, float LowerThreshold, float UpperThreshold){
   else return 0;
 }
   
-int batteryIsOk(float temp, float state, float charge) {
+int batteryIsOk(float Temperature, float StateOfCharge, float RateOfCharge) {
   int BatteryCondition = 0;
-  BatteryCondition = !(CheckRange(temp,0,45) || CheckRange(state,20,80) || CheckRange(charge,0,0.8));
+  BatteryCondition = !(CheckRange(Temperature,0,45) || CheckRange(StateOfCharge,20,80) || CheckRange(RateOfCharge,0,0.8));
   return BatteryCondition;
 }
 
@@ -20,7 +20,7 @@ int main() {
   
   //Checking Boundary conditions
   assert(batteryIsOk(1, 79, 0.7) == 1); //battery condition is fine
-  
+  assert(batteryIsOk(45, 80, 0.8) == 1); //battery condition is fine
   assert(batteryIsOk(0, 70, 0.9) == 0); //battery condition is not fine
   
   assert(batteryIsOk(50, 85, 1) == 0);  //battery condition is not fine
