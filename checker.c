b70791bb640525a9e44061e67f5b6787f14ae1ce
SudeshnaMukherjee94 <91111960+SudeshnaMukherjee94@users.noreply.github.com>
2022-02-11 01:32:29 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -33,5 +33,14 @@ int batteryIsOk(float temp, float state, float charge) {
 
 int main() {
   assert(batteryIsOk(25, 70, 0.7) == 1); //battery condition is fine
+  
+  //Checking Boundary conditions
+  assert(batteryIsOk(1, 79, 0.7) == 1); //battery condition is fine
+  assert(batteryIsOk(45, 80, 0.8) == 0); //battery condition is not fine
+  assert(batteryIsOk(0, 70, 0.9) == 0); //battery condition is not fine
+  
   assert(batteryIsOk(50, 85, 1) == 0);  //battery condition is not fine
+  assert(batteryIsOk(25, 85, 1) == 0);  //battery condition is not fine
+  assert(batteryIsOk(50, 70, 1) == 0);  //battery condition is not fine
+  assert(batteryIsOk(50, 85, 0.7) == 0);  //battery condition is not fine
 }
